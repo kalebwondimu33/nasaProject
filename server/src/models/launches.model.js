21287eaa71d97404f77b0ec49815f7174cb1f180
kaleb <kalebwondimu33@gmail.com>
2026-05-08 19:54:18 +0300

    node api done for the nasa frontend

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,43 @@
+const launches = new Map();
+let latestFlightNumber = 100;
+const launch = {
+  flightNumber: 100,
+  mission: "kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 20230"),
+  target: "Kepler-442 b",
+  customer: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
+};
+launches.set(launch.flightNumber, launch);
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customer: ["ZTM", "NASA"],
+      upcoming: true,
+      success: true,
+    }),
+  );
+}
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+module.exports = {
+  existsLaunchWithId,
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
+};
